ebf0e837686e9458be89f7c873bbbd83f162d9ae
VatsalD12 <vdedhia12@gmail.com>
2022-11-01 02:17:56 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '795e44bb-fed0-43da-8d07-e51579522dfa';
+export const MODAL_CLOSE_SEC = 2.5;
